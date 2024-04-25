1a0b4beced0659d7c081eb9ff859899174b2c2c0
Aleksey Pardaev <lpardaev607@gmail.com>
2024-04-25 19:30:16 +0300

    1

--- lesson1/src/main/resources/SOLUTION.sql
+++ lesson1/src/main/resources/SOLUTION.sql
@@ -1,8 +1,8 @@
-CREATE TABLE Student(id bigint, name varchar, birthday date, group int, PRIMARY KEY(id));
+CREATE TABLE Student(id bigint, name varchar(255), birthday date, group int, PRIMARY KEY(id));
 
-CREATE TABLE Subject(id bigint, name varchar, description varchar, grade int, PRIMARY KEY(id));
+CREATE TABLE Subject(id bigint, name varchar(255), description varchar(255), grade int, PRIMARY KEY(id));
 
-CREATE TABLE PaymentType(id bigint, name varchar, PRIMARY KEY(id));
+CREATE TABLE PaymentType(id bigint, name varchar(255), PRIMARY KEY(id));
 
 CREATE TABLE Payment(id bigint, type_id bigint, amount decimal, student_id bigint, payment_date datetime, PRIMARY KEY(id), FOREIGN KEY(type_id) REFERENCES PaymentType(id), FOREIGN KEY(student_id) REFERENCES Student(id));
 
